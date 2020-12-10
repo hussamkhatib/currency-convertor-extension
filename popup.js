c81af2bda5db6fba55a02878968877d26d9687bc
hussamkhatib <hussamkhatib20@gmail.com>
2020-12-10 12:07:05 +0530

    creating boilterplate for the project

--- popup.js
+++ popup.js
@@ -1,13 +1,14 @@
-import "./styles.css";
 let create = document.querySelector("#create");
 let wrapper = document.querySelector("#flexContainer");
 
-create.addEventListener("click", createElement);
 const btn = document.createElement("button");
+create.addEventListener("click", createElement);
+btn.addEventListener("click", createNewCurrency);
+
 function createElement() {
+  const div = document.createElement("div");
   const dropdown = document.createElement("select");
   const fragment = document.createDocumentFragment();
-  const div = document.createElement("div");
   btn.innerText = "okay";
   for (let i = 0; i < 3; i++) {
     const option = document.createElement("option");
@@ -18,9 +19,10 @@ function createElement() {
   div.appendChild(dropdown);
   div.appendChild(btn);
   div.className = "container";
-  insertBefore(div, create);
+  wrapper.replaceChild(div, create);
 }
 
-function insertBefore(el, referenceNode) {
-  referenceNode.parentNode.insertBefore(el, referenceNode);
+function createNewCurrency() {
+  let lastDiv = wrapper.querySelector("div:last-of-type");
+  wrapper.replaceChild(create, lastDiv);
 }
