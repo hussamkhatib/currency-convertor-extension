184a42e4c62edcdd98f460e50c7d4eed6d4c9604
hussamkhatib <hussamkhatib20@gmail.com>
2020-12-16 11:09:02 +0530

    adding web branch

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 let create = document.querySelector("#create");
 let wrapper = document.querySelector("#flexContainer");
+const dropdown = document.createElement("select");
 
-const btn = document.createElement("button");
+const okay = document.createElement("button");
 create.addEventListener("click", createElement);
-btn.addEventListener("click", createNewCurrency);
+// +
+okay.addEventListener("click", createNewCurrency);
 
 function createElement() {
   const div = document.createElement("div");
-  const dropdown = document.createElement("select");
   const fragment = document.createDocumentFragment();
-  btn.innerText = "okay";
+  okay.innerText = "okay";
+  dropdown.textContent = "";
   for (let i = 0; i < 3; i++) {
     const option = document.createElement("option");
     option.value = option.innerText = i;
@@ -17,12 +19,16 @@ function createElement() {
   }
   dropdown.appendChild(fragment);
   div.appendChild(dropdown);
-  div.appendChild(btn);
+  div.appendChild(okay);
   div.className = "container";
   wrapper.replaceChild(div, create);
 }
 
 function createNewCurrency() {
   let lastDiv = wrapper.querySelector("div:last-of-type");
-  wrapper.replaceChild(create, lastDiv);
+  wrapper.replaceChild(okay, lastDiv);
+  wrapper.appendChild(create);
+  var value = dropdown.options[dropdown.selectedIndex].value;
+  var text = dropdown.options[dropdown.selectedIndex].text;
+  console.log(value, text);
 }
