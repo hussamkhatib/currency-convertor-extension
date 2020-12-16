13e1b63cbc626bc3ebd609623c0deda0eeb1ce2a
hussamkhatib <hussamkhatib20@gmail.com>
2020-12-16 12:37:41 +0530

    adding REST_API,adding simple DOM manipulation

--- index.js
+++ index.js
@@ -2,19 +2,39 @@ let create = document.querySelector("#create");
 let wrapper = document.querySelector("#flexContainer");
 const dropdown = document.createElement("select");
 
+const URL = 'https://api.exchangeratesapi.io/latest'
 const okay = document.createElement("button");
-create.addEventListener("click", createElement);
-// +
+let currencyName = [ ]
+let currencyValue = [ ]
+let fetched = false
+
 okay.addEventListener("click", createNewCurrency);
 
+
+//        currencyName = data.rates
+
+  //      console.log(Object.keys(currencyName))
+
+fetchOptions()
+async function fetchOptions() {
+    const resp = await fetch(URL)
+    const data = await resp.json()
+    currencyName = Object.keys(data.rates)
+}
+
+
+const div = document.createElement("div");
+create.addEventListener("click", createElement);
+
+// plus
 function createElement() {
-  const div = document.createElement("div");
   const fragment = document.createDocumentFragment();
   okay.innerText = "okay";
   dropdown.textContent = "";
-  for (let i = 0; i < 3; i++) {
+  div.innerText =''
+  for (let i = 0; i < currencyName.length; i++) {
     const option = document.createElement("option");
-    option.value = option.innerText = i;
+    option.value = option.innerText = currencyName[i];
     fragment.appendChild(option);
   }
   dropdown.appendChild(fragment);
@@ -24,11 +44,16 @@ function createElement() {
   wrapper.replaceChild(div, create);
 }
 
-function createNewCurrency() {
-  let lastDiv = wrapper.querySelector("div:last-of-type");
-  wrapper.replaceChild(okay, lastDiv);
-  wrapper.appendChild(create);
-  var value = dropdown.options[dropdown.selectedIndex].value;
-  var text = dropdown.options[dropdown.selectedIndex].text;
-  console.log(value, text);
+
+function createNewCurrency(e) {
+    e.stopPropagation() 
+    let text = dropdown.options[dropdown.selectedIndex].text;
+    let lastDiv = wrapper.querySelector("div:last-of-type");
+  //  const div = document.createElement('div')
+   // div.className = 'container'
+    div.innerText = text
+    console.log(lastDiv)
+    wrapper.replaceChild(div, lastDiv);
+    wrapper.appendChild(create);
+
 }
