96f16b119c8816a1b92918ad7dc740e2d16c52b2
hussamkhatib <hussamkhatib20@gmail.com>
2020-12-17 12:41:02 +0530

    fixing convert function

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const URL = 'https://api.exchangeratesapi.io/latest'
 const okay = document.createElement("button");
 
 let currencyArray = [ ]
+let currencyArray2 = [ ]
 let currencyValue = [ ]
 let activeARR = [['EUR',1.00]]
 let keys = []
@@ -17,16 +18,16 @@ const trashBin = `<svg width="13" height="15" viewBox="0 0 13 15" fill="none" xm
 <path d="M0.625 3.75V15H11.875V3.75H0.625ZM3.75 12.5C3.75 12.845 3.47 13.125 3.125 13.125C2.78 13.125 2.5 12.845 2.5 12.5V6.25C2.5 5.905 2.78 5.625 3.125 5.625C3.47 5.625 3.75 5.905 3.75 6.25V12.5ZM6.875 12.5C6.875 12.845 6.595 13.125 6.25 13.125C5.905 13.125 5.625 12.845 5.625 12.5V6.25C5.625 5.905 5.905 5.625 6.25 5.625C6.595 5.625 6.875 5.905 6.875 6.25V12.5ZM10 12.5C10 12.845 9.72 13.125 9.375 13.125C9.03 13.125 8.75 12.845 8.75 12.5V6.25C8.75 5.905 9.03 5.625 9.375 5.625C9.72 5.625 10 5.905 10 6.25V12.5ZM12.5 1.25V2.5H0V1.25H3.56937C4.13187 1.25 4.58875 0.563125 4.58875 0H7.91062C7.91062 0.563125 8.36688 1.25 8.93 1.25H12.5Z" fill="black"/>
 </svg>
 `
-
 okay.addEventListener("click", createNewCurrency);
 
 fetchOptions()
 async function fetchOptions() {
     const resp = await fetch(URL)
     const data = await resp.json()
-    currencyArray = Object.entries(data.rates)
-   //console.log(currencyArray)
+    currencyArray = [['EUR',1.00],...Object.entries(data.rates)]
+    currencyArray2 = [...currencyArray]
     currencyValue = data.rates
+    currencyValue =  Object.assign({},{'EUR':1.00},currencyValue);
     keys = Object.keys(currencyValue)
 }
 
@@ -52,7 +53,6 @@ function createElement() {
   wrapper.replaceChild(div, create);
 }
 
-
 function createNewCurrency() {
   const div = document.createElement("div");
   const p = document.createElement('p')
@@ -77,24 +77,23 @@ function createNewCurrency() {
   inputFields = document.querySelectorAll('input')  
   input.addEventListener('change',convert)
   const index = keys.map(e => e).indexOf(text);
-  activeARR.push(currencyArray[index])
+  activeARR.push(currencyArray2[index])
+ 
 }
 
 function convert(e) {
   let curValue = e.target.value
   let curName = e.target.parentNode.children[0].innerText
-  const index = activeARR.map(e => e[0]).indexOf(curName);
+  const index = currencyArray.map(e => e[0]).indexOf(curName);
+  let diff = curValue/currencyArray[index][1]
 
-  let diff = curValue/activeARR[index][1]
-
-  activeARR.forEach((inp,index) => {
-    if(curValue !== inputFields[index].value) { 
-      inp[1] *= diff 
-      inputFields[index].value = inp[1]
-    }else{
-      activeARR[index][1] = curValue
-    }
+  currencyArray2.forEach((inp,index) => {
+    inp[1] = diff * currencyArray2[index][1] 
+  })
+  
+  activeARR.forEach((inp,i) => {
+    let index = currencyArray2.map(e => e[0]).indexOf(inp[0]);
+    inputFields[i].value = currencyArray2[index][1]
 })
-console.log(activeARR)
 }
 
